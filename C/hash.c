25d771b7a9b5acddcd281cd3e54ae08d0e38ae23
Michael Rinos <mrinos98@gmail.com>
2017-03-21 11:23:57 -0400

    Found some old code which gave me an idea about a better way of doing this, started refactoring hash.*

--- C/hash.c
+++ C/hash.c
@@ -6,138 +6,46 @@
 #include <string.h>
 
 
+Table* create(long (*hash)(void* key),
+              bool (*equals)(void* key1, void* key2),
+              void (*print)(void* key1, void* key2)){
 
-/* Create a new hashtable which contains nodes */
-hashtable_n *htn_create( int size ) {
-
-	hashtable_n *hashtable = NULL;
-	int i;
-
-	if( size < 1 ) return NULL;
-
-	/* Allocate the table itself. */
-	if( ( hashtable = malloc( sizeof( hashtable_n ) ) ) == NULL ) {
-		return NULL;
-	}
-
-	/* Allocate pointers to the head nodes. */
-	if( ( hashtable->table = malloc( sizeof( entry_n * ) * size ) ) == NULL ) {
-		return NULL;
-	}
-	for( i = 0; i < size; i++ ) {
-		hashtable->table[i] = NULL;
-	}
-
-	hashtable->size = size;
-
-	return hashtable;	
+    Table* myTable =(Table*)malloc(sizeof(Table));
+    if (myTable==NULL){
+        assert(NULL);
+    }
+    myTable-> table = (Entry**)calloc(INITIAL_CAPACITY, sizeof(Entry*));
+    myTable-> size = 0;
+    myTable-> capacity = INITIAL_CAPACITY;
+    myTable-> hash = hash;
+    myTable-> equals = equals;
+    myTable-> print = print;
+    
+    return myTable;
 }
 
-/* Hash a string for a particular hash table. */
-int htn_hash( hashtable_n *hashtable, char *key ) {
-
-	unsigned long int hashval;
-	size_t i = 0;
-
-	/* Convert our string to an integer */
-	while( hashval < ULONG_MAX && i < strlen( key ) ) {
-		hashval = hashval << 8;
-		hashval += key[ i ];
-		i++;
-	}
-
-	return hashval % hashtable->size;
+void * htn_set( hashtable_n *hastable, void * key, void * value){
+    
 }
 
-/* Create a key-value pair.
- * Containing a char * as the key and a Node as a value
- **/
-entry_n *htn_newpair( char *key, Node * value ) {
-	entry_n *newpair;
+void * htn_get( hashtable_n * hashtable, void * key){
+    long hash = hashtable->hash(key);
+    if (hashtable->table[key] == NULL){
+        assert(NULL);
+    }
 
-	if( ( newpair = malloc( sizeof( entry_n ) ) ) == NULL ) {
-		return NULL;
-	}
-
-	if( ( newpair->key = strdup( key ) ) == NULL ) {
-		return NULL;
-	}
-
-	if( ( newpair->value = value ) == NULL ) {
-		return NULL;
-	}
-
-	newpair->next = NULL;
-
-	return newpair;
+    if (hashtable->size >= hashtable.capacity * LOAD_THRESHOLD){
+        Entry ** oldTable = hashtable->table;
+        size_t oldSize = hashtable->size;
+        hashtable->capacity = oldSize * RESIZE_FACTOR;
+        hashtable->table = (Entry**) calloc(hashtable->capacity, sizeof(Entry*));
+        for (size_t i = 0; i <= oldSize; i++){  //Wrong
+            htn_set(hashtable. 
+        }
+    }
 }
 
-/* Insert a key-value pair into a hash table. */
-void htn_set( hashtable_n *hashtable, char *key, Node * value ) {
-	int bin = 0;
-	entry_n *newpair = NULL;
-	entry_n *next = NULL;
-	entry_n *last = NULL;
+bool has(hashtable_n * hashtable, void * key){
 
-	bin = htn_hash( hashtable, key );
-
-	next = hashtable->table[ bin ];
-
-	while( next != NULL && next->key != NULL && strcmp( key, next->key ) > 0 ) {
-		last = next;
-		next = next->next;
-	}
-
-	/* There's already a pair.  Let's replace that string. */
-	if( next != NULL && next->key != NULL && strcmp( key, next->key ) == 0 ) {
-
-//		free( next->value );            // I should call delete not free here
-        destroy_node(next->value);
-		next->value = value;
-
-	/* Nope, could't find it.  Time to grow a pair. */
-	} else {
-		newpair = htn_newpair( key, value );
-
-		/* We're at the start of the linked list in this bin. */
-		if( next == hashtable->table[ bin ] ) {
-			newpair->next = next;
-			hashtable->table[ bin ] = newpair;
-	
-		/* We're at the end of the linked list in this bin. */
-		} else if ( next == NULL ) {
-			last->next = newpair;
-	
-		/* We're in the middle of the list. */
-		} else  {
-			newpair->next = next;
-			last->next = newpair;
-		}
-	}
 }
-
-/* Retrieve a key-value pair from a hash table. */
-Node *htn_get( hashtable_n *hashtable, char *key ) {
-	int bin = 0;
-	entry_n *pair;
-
-	bin = htn_hash( hashtable, key );
-
-	/* Step through the bin, looking for our value. */
-	pair = hashtable->table[ bin ];
-	while( pair != NULL && pair->key != NULL && strcmp( key, pair->key ) > 0 ) {
-		pair = pair->next;
-	}
-
-	/* Did we actually find anything? */
-	if( pair == NULL || pair->key == NULL || strcmp( key, pair->key ) != 0 ) {
-		return NULL;
-
-	} else {
-		return pair->value;
-	}
-	
-}
-
-
 
--- C/hash.h
+++ C/hash.h
@@ -2,47 +2,30 @@
 #define HASH_H
 #include "Node.h"
 
-typedef struct entry_n entry_n;
-struct entry_n {
+typedef struct entry_n {
     char * key;
     Node * value;
     entry_n * next;
-};
-/**
-typedef struct entry_i entry_i;
-struct entry_i {
-    int * value;
-    char * key;
-    entry_i * next;
-};
-**/
+}Entry;
 
-typedef struct hashtable_n hashtable_n;
-struct hashtable_n{
-    int size;
+typedef struct hashtable_n{
+    size_t size;
+    size_t capacity;
+    long (*hash)(void* key);  ///< hash function for key
+    bool (*equals)(void *key1, void* key2); ///< equals function for key comparison
+    void (*print)(void *key, void* value);  ///< print function for table dump debug
     entry_n ** table;
-};
-/**
-typedef struct hashtable_i hashtable_i;
-struct hastable_i{
-    int size;
-    entry_i ** table;
-};
-**/
+}Table;
 
-hashtable_n * htn_create (int size);
-//hashtable_i * hti_create (int size);
+Table* create(long (*hash)(void* key),
+              bool (*equals)(void* key1, void* key2),
+              void (*print)(void* key1, void* key2));
 
-int htn_hash( hashtable_n * hashtable, char * key);
-//int hti_hash( hashtable_i * hashtable, char * key);
 
-entry_n * htn_newpair( char * key, Node * value);
-//entry_i * hti_newpair( char * key, int value);
+void * htn_set( hashtable_n *hastable, void * key, void * value);
 
-void htn_set( hashtable_n *hastable, char * key, Node * value);
-//void hti_set( hashtable_i *hastable, char * key, int value);
+void * htn_get( hashtable_n * hashtable, void * key);
 
-Node * htn_get( hashtable_n * hashtable, char * key);
-//int hti_get( hashtable_i * hashtable, char * key);
+bool has(hashtable_n * hashtable, void * key);
 
 #endif
